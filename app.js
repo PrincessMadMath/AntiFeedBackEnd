ae57a9e0810f69adc6d00b3ed1b35614fb8edfc7
Samuel <samuel_lavoie@hotmail.com>
2017-01-21 13:31:23 -0500

    Change test

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
   res.send('Hello World!')
+})
+
+app.post('/search', function (req, res, next) {
+  const body = req.body;
 })
 
 app.listen(3000, function () {
