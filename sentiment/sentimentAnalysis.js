32300a438b07c28756a612f4e4c3b4c2888d8ec0
Mathieu Gamache <mathg99@hotmail.com>
2017-01-22 01:27:37 -0500

    Deep learning for analysis
    lol

--- sentiment/sentimentAnalysis.js
+++ sentiment/sentimentAnalysis.js
@@ -2,6 +2,7 @@ const rp = require('request-promise');
 const SENTIMENT_URL = 'https://api.beta.yaas.io/sap/ta-sentiments/v1';
 const tokenManager = require('./sapToken');
 const _ = require('lodash');
+const factor = require('./factor.json');
 
 module.exports = {
     analyseText
@@ -16,56 +17,6 @@ function analyseText(text)
         console.log(JSON.stringify(sentimentAnalys));
         return sentimentAnalys;
     });
-}
-
-function analyseResponse(text, response)
-{
-    let isStrongPositive = _.find(response.entities, function(o) { 
-        return o.label.toLowerCase().includes("strongpositive")}) !== undefined;
-    let isStrongNegative = _.find(response.entities, function(o) { 
-        return o.label.toLowerCase().includes("strongnegative")}) !== undefined;
-    let isWeakPositive = _.find(response.entities, function(o) { 
-        return o.label.toLowerCase().includes("positive")}) !== undefined;
-    let isWeakNegative = _.find(response.entities, function(o) { 
-        return o.label.toLowerCase().includes("negative")}) !== undefined;
-
-    if(isStrongPositive)
-    {
-        return {
-            "text": text,
-            "sentiment": "positive",
-            "strength" : "strong"
-        }
-    }
-    if(isStrongNegative)
-    {
-        return {
-            "text": text,
-            "sentiment": "negative",
-            "strength" : "strong"
-        }
-    }
-    if(isWeakPositive)
-    {
-        return {
-            "text": text,
-            "sentiment": "positive",
-            "strength" : "weak"
-        }
-    }
-    if(isWeakNegative)
-    {
-        return {
-            "text": text,
-            "sentiment": "negative",
-            "strength" : "weak"
-        }
-    }
-
-    return {
-        "text": text,
-        "sentiment": "neutral"
-    }
 }
 
 
@@ -104,3 +55,128 @@ function post(text)
     }
     );     
 }
+
+function analyseResponse(text, response)
+{
+    let score = getSentimentScore(response.entities);
+
+    if(score > factor.StrongTrigger)
+    {
+        return {
+            "text": text,
+            "sentiment": "positive",
+            "strength" : "strong"
+        }
+    }
+
+    if(score > factor.WeakTrigger)
+    {
+        return {
+            "text": text,
+            "sentiment": "positive",
+            "strength" : "weak"
+        }
+    }
+
+    if(score < factor.StrongTrigger)
+    {
+        return {
+            "text": text,
+            "sentiment": "negative",
+            "strength" : "strong"
+        }
+    }
+
+    if(score < -factor.WeakTrigger)
+    {
+        return {
+            "text": text,
+            "sentiment": "negative",
+            "strength" : "weak"
+        }
+    }
+
+
+    return {
+        "text": text,
+        "sentiment": "neutral"
+    }
+}
+
+function getSentimentScore(entities)
+{
+    let score = 0;
+    score += getScoreForPredicate(entities, factor.StrongPositive, strongPositiveDetector);
+    score += getScoreForPredicate(entities, factor.StrongNegative, strongNegativeDetector);
+    score += getScoreForPredicate(entities, factor.WeakPositive, weakPositiveDetector);
+    score += getScoreForPredicate(entities, factor.WeakNegative, weakNegativeDetector);
+    score += getScoreForPredicate(entities, factor.MinorProblem, minorProblemDetector);
+    score += getScoreForPredicate(entities, factor.MajorProblem, majorProblemDetector);
+    score += getScoreForPredicate(entities, factor.AmbigousProfanity, ambigousProfanityDetector);
+    score += getScoreForPredicate(entities, factor.UnambigousProfanity, unAmbigousProfanityDetector);
+    return score;
+}
+
+function getScoreForPredicate(entities, factor, predicate)
+{
+    let list = entities.filter(predicate)
+    let listCount = list !== undefined ? list.length : 0;
+    console.log(listCount + " : " + factor);
+    return listCount * factor;
+}
+
+function strongPositiveDetector(entry)
+{
+    if(entry.text.toLowerCase().includes('trump'))
+    {
+        return false;
+    }
+
+    return entry.label.toLowerCase().includes("strongpositive");
+}
+
+function strongNegativeDetector(entry)
+{
+    if(entry.text.toLowerCase().includes('trump'))
+    {
+        return false;
+    }
+
+    return entry.label.toLowerCase().includes("strongnegative");
+}
+
+function weakPositiveDetector(entry)
+{
+    return entry.label.toLowerCase().includes("weakpositive");
+}
+
+function weakNegativeDetector(entry)
+{
+    return entry.label.toLowerCase().includes("weaknegative");
+}
+
+
+function minorProblemDetector(entry)
+{
+    return entry.label.toLowerCase().includes("minorproblem");
+}
+
+function majorProblemDetector(entry)
+{
+    return entry.label.toLowerCase().includes("majorproblem");
+}
+
+function ambigousProfanityDetector(entry)
+{
+    return entry.label.toLowerCase().includes("ambiguous_profanity");
+}
+
+function unAmbigousProfanityDetector(entry)
+{
+    return entry.label.toLowerCase().includes("unambiguous_profanity");
+}
+
+
+
+
+
