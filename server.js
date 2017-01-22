b0c750d6147682fdbc695fe0df945bdb23772981
Anthony Garant <atgarant@gmail.com>
2017-01-22 10:04:41 -0500

    getting tokens

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const passport = require('passport');
 const TwitterStrategy = require('passport-twitter').Strategy;
 const appToken = require('./appToken');
-const routes = require('./routes.js');
 const bodyParser = require('body-parser');
 const secretCache = require('./SecretCache');
 const app = express();
+const search = require('./search');
+const rp = require('request-promise');
 
 
 passport.use(new TwitterStrategy({
@@ -14,6 +15,7 @@ passport.use(new TwitterStrategy({
       callbackURL: process.env.TWITTER_CB_URL
     },
     function(token, tokenSecret, profile, cb) {
+      console.log(token);
       secretCache.add(profile.id, token, tokenSecret);
       return cb(null, profile);
     }));
@@ -36,10 +38,66 @@ app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveU
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', function (req, res, next) {
+  res.status(200);
+  res.send('Hello World!')
+});
+
+app.get('/failed_login', function (req, res, next) {
+  res.status(403);
+  res.send('Twitter authentication failed');
+});
+
+app.get('/login', function (req, res) {
+  res.status(403);
+  res.send('Twitter authentication required');
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.post('/auth/twitter/callback', function(req, res){
+  req.session.oauth_token = req.body.oauth_token;
+  req.session.oauth_verifier = req.body.oauth_verifier;
+  var options = {
+    method: 'POST',
+    uri: 'https://api.twitter.com/oauth/access_token',
+    form: req.body,
+    json: true
+  };
+  rp(options)
+    .then(function (body) {
+      // POST succeeded...
+      console.log(res);
+      res.sendStatus(200);
+    })
+    .catch(function (err) {
+      // POST failed...
+      console.log(err);
+    });
+});
+
+app.get('/api/feed',
+  function (req, res, next) {
+    return res.sendStatus(200);
+    const body = req.body;
+    search.feed(req.session.id)
+      .then(feed => res.send(feed));
+  }
+);
+
+app.post('/api/compare',
+  function (req, res, next) {
+    const body = req.body;
+    search.getTweets({q:body.query, lang:'en', count:100})
+      .then(tweets => res.send(tweets));
+  }
+);
+
+
+
 app.listen(process.env.PORT || 3000, function () {
   appToken.renew()
   .then(() => {
     console.log('Express listening on port ' + process.env.PORT + ' or 3000');
-    routes.init(app, passport);
-  })
+  });
 });
